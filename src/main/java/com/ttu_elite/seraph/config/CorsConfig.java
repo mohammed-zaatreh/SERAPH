958ebf9f8e6806616235d62688be0ccc83c8e4d2
mohamad asha <mohamad.asha@welab.health>
2026-01-07 21:25:22 +0300

    allowed multi domain request fixed v2

--- src/main/java/com/ttu_elite/seraph/config/CorsConfig.java
+++ src/main/java/com/ttu_elite/seraph/config/CorsConfig.java
@@ -8,8 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Apply to ALL endpoints
-                .allowedOrigins("*") // Allow ALL origins (React, Postman, etc.)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
